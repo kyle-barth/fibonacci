4538ec05d88c984956e815335286828f5f47b112
kyle-barth <40353515+kyle-barth@users.noreply.github.com>
2018-06-19 12:16:42 +0100

    Add files via upload

--- fib.js
+++ fib.js
@@ -2,6 +2,6 @@ var x=1;
 var arr = [];
 for (var i = 0; i < 20; i++){
   arr[i] = x;
-  if (arr.length > 2) x = arr[i] + arr[i-1];
-  console.log(x);
+  if (arr.length > 1) x = arr[i] + arr[i-1];
 }
+console.log(arr.toString());
